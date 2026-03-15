8b7097efb0c11684ad702740ef3faed66daabe4f
fish30008 <146107079+fish30008@users.noreply.github.com>
2026-03-15 17:46:42 +0200

    final_version

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: '2ee5f01b5f292b184251f9124588be23d0ae34ab', queries,  });
 export default client;
   
